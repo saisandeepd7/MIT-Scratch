62cf25e73e15912c80b75a315c188139e27a3f31
Sai Sandeep <sandeep@qpiai.tech>
2023-12-27 11:23:04 +0530

    intial commit

---
+++ src/components/SidebarConstants.js
@@ -0,0 +1,22 @@
+// Components List
+export const motionComponents = [
+  "MOVE",
+  "MOVE_Y",
+  "TURN_CLOCKWISE",
+  "TURN_ANTI_CLOCKWISE",
+];
+
+export const looksComponents = [
+  "SAY_MESSAGE",
+  "SAY_MESSAGE_WITH_TIMER",
+  "THINK",
+  "THINK_TIMER",
+  "HIDE_MESSAGE",
+  "SIZE",
+  "SHOW",
+  "HIDE",
+];
+
+export const eventsComponents = ["BROADCAST"];
+
+export const controlComponents = ["WAIT", "REPEAT"];
